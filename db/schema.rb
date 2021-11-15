1ed36e2755961f6e0ca5add0d8ab2066059a1283
Alena Bauer <alena.a.bauer@gmail.com>
2021-11-15 16:20:31 +0100

    generate the Reservation model and add validations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_15_151424) do
+ActiveRecord::Schema.define(version: 2021_11_15_151845) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,19 @@ ActiveRecord::Schema.define(version: 2021_11_15_151424) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_offers_on_user_id"
+  end
+
+  create_table "reservations", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "offer_id", null: false
+    t.datetime "start_date"
+    t.datetime "end_date"
+    t.string "status", default: "Pending"
+    t.float "total_price"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["offer_id"], name: "index_reservations_on_offer_id"
+    t.index ["user_id"], name: "index_reservations_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -42,4 +55,6 @@ ActiveRecord::Schema.define(version: 2021_11_15_151424) do
   end
 
   add_foreign_key "offers", "users"
+  add_foreign_key "reservations", "offers"
+  add_foreign_key "reservations", "users"
 end
