a3934474157b28910b5238dcd5b92dee232deab5
Alena Bauer <alena.a.bauer@gmail.com>
2021-11-15 16:15:37 +0100

    generate the Offer model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :offers
+
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :address, presence: true
