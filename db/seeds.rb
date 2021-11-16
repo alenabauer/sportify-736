d421c40d2c3484e7414061e07aeb9549d6a16543
Emily Martinek <emily.martinek12@gmail.com>
2021-11-16 12:11:05 +0100

    created index route controller view and seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+user_1 = User.create(email: "bob@bob.com", password: "123456", first_name: "bob", last_name: "jones", address: "123 way")
+user_2 = User.create(email: "suzy@suzy.com", password: "78910", first_name: "suzy", last_name: "johnson", address: "456 olive")
+user_3 = User.create(email: "peter@peter.com", password: "111213", first_name: "peter", last_name: "jeff", address: "789 lane")
+
+40.times do
+  offer = Offer.new(
+    name: Faker::Games::Pokemon.name,
+    equipment_type: Faker::Creature::Animal.name,
+    description: Faker::TvShows::StrangerThings.quote,
+    price: rand(1..500),
+    equipment_category: CATEGORIES.sample,
+    user: User.all.sample
+  )
+  offer.save!
+end
+
+puts "Offers created"
