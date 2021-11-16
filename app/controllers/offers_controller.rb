474b75d8b62a219c90c8ee7b751badd365d2d36e
paula garcia <paula.garciadv@gmail.com>
2021-11-16 14:13:35 +0100

    new and create functions

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,27 +1,27 @@
 class OffersController < ApplicationController
 
-  def new
-    @offer = Offer.new
-  end
+  # def new
+  #   @offer = Offer.new
+  # end
 
-  # GET /offers/1/edit
-  def edit
-  end
+  # # GET /offers/1/edit
+  # def edit
+  # end
 
-  # POST /offers
-  def create
-    @offer = Offer.new(offer_params)
+  # # POST /offers
+  # def create
+  #   @offer = Offer.new(offer_params)
 
-    if @offer.save
-      redirect_to @offer, notice: 'Offer was successfully created.'
-    else
-      render :new
-    end
-  end
+  #   if @offer.save
+  #     redirect_to @offer, notice: 'Offer was successfully created.'
+  #   else
+  #     render :new
+  #   end
+  # end
 
-  private
+  # private
 
-  def offer_params
-    params.require(:offer).permit(:name, :description, :equipment_type, :equipment_category, :price)
-  end
+  # def offer_params
+  #   params.require(:offer).permit(:name, :description, :equipment_type, :equipment_category, :price)
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :offers
+  get 'offers/new', to: 'offers#new', as: :new_offer
+  post '/offers', to: 'offers#create'
+  get 'offers/:id/edit', to: 'offers#edit', as: :edit_offer
+  patch '/offers/:id', to: 'offers#update'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
