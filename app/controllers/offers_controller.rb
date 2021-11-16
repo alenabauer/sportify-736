db18fa531e60bcb95cc7c966c81115080c092abc
Emily Martinek <emily.martinek12@gmail.com>
2021-11-16 14:12:18 +0100

    added index to controller

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -2,4 +2,8 @@ class OffersController < ApplicationController
   def show
     @offer = Offer.find(params[:id])
   end
+
+  def index
+    @offers = Offer.all
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/offers', to: 'pages#index', as: :offers
+  get '/offers', to: 'offers#index', as: :offers
 end
