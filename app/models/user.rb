5b9e4b0a3db73070c9c6e20eb8de6933a363858d
Alena Bauer <alena.a.bauer@gmail.com>
2021-11-16 08:48:10 +0100

    add - user has_many reservations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :offers
+  has_many :reservations
 
   validates :first_name, presence: true
   validates :last_name, presence: true
