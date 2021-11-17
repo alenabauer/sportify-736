8ab10adc34ec281f86ac9c5bafab69bcbab7ca29
Alena Bauer <alena.a.bauer@gmail.com>
2021-11-17 12:05:43 +0100

    add markers in the index action in the offers_controller

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -37,6 +37,15 @@ class OffersController < ApplicationController
 
   def index
     @offers = Offer.all
+
+    @markers = @offers.map do |offer|
+      next unless offer.user.geocoded?
+
+      {
+        lat: offer.user.latitude,
+        lng: offer.user.longitude
+      }
+    end
   end
 
   private
